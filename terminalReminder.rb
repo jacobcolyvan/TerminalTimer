b20d3c93eeda43af9a9da518dc9c5807fb60b7a8
Jacob Colyvan <jacob_colyvan@live.com>
2020-04-12 19:20:47 +1000

    added basic command line timer functionality

---
+++ terminalReminder.rb
@@ -0,0 +1,23 @@
+# demands command line Arguments 
+timerLength = ARGV
+#exits program if given no arguments
+if timerLength == []
+    exit
+end
+timerLength = timerLength[0].to_f
+timerLengthinSeconds = (timerLength*60).to_i
+
+# sends system notification through apple script notifying that timer has started
+fork { exec ( "osascript -e 'display notification \"#{timerLength} minute timer is starting\" with title \"Timer starting\"'" ) }
+# timer loop that notifies command line every 10 seconds.
+while timerLengthinSeconds > 0
+    if timerLengthinSeconds%10 == 0;
+        puts "There are #{timerLengthinSeconds} seconds left"
+    end
+    timerLengthinSeconds -= 1
+    sleep(1)
+end
+
+
+# sends system notification through apple script when timer is up
+fork { exec ( "osascript -e 'display notification \"#{timerLength} minutes is up\" with title \"Timer done\"'" ) }
